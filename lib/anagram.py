ea8066078831843c77c584eff63775c63daf989c
Mohammed <khamismitch@gmail.com>
2024-01-29 10:43:16 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.anagrams = []
+
+    def match(self, words):
+        for word in words:
+            if self.is_anagram(word):
+                self.anagrams.append(word)
+        return self.anagrams
+
+    def is_anagram(self, word):
+        return sorted(self.word) == sorted(word.lower())
